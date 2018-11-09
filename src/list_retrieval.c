a3e3aa47e05fb8ebc753e223cca187338035f379
Gus Castles <guscastles@gmail.com>
2018-11-09 19:48:53 +1100

    Refactored list_transformation, added list_retrieval

---
+++ src/list_retrieval.c
@@ -0,0 +1,14 @@
+#include "list.h"
+#include <malloc.h>
+#include <limits.h>
+
+int element_at(List list, int position) {
+   if (OUT_OF_LIMITS(position, list.size))
+      return INT_MIN;
+   return list.list[position];
+}
+
+int list_size(List list) {
+   return list.size;
+}
+
